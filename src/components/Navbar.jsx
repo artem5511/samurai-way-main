2036dbb9e532621ff766fa758569d9433f2c3104
lui viton <artem_5511@mail.ru>
2022-04-05 01:13:49 +0300

    new components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Navbar = () => {
     return <nav className='nav'>
-    <img src='https://img1.akspic.ru/previews/7/8/6/7/6/167687/167687-pesok-senokosnoye_ugodye-ekoregion-lazurnyj-vodoem-360x640.jpg' alt='#'/>
+    {/*<img src='https://img1.akspic.ru/previews/7/8/6/7/6/167687/167687-pesok-senokosnoye_ugodye-ekoregion-lazurnyj-vodoem-360x640.jpg' alt='#'/>*/}
         <div>
             <a>Profile</a>
         </div>
