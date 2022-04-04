fdf0d1275c4ef1b59958642d4045726e39c5fb2a
lui viton <artem_5511@mail.ru>
2022-04-05 00:28:53 +0300

    new components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header =()=> {
+    return <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhUfCr5fEdjMLf6ybpi0NoXFBIYKNiVgdzMw&usqp=CAU'/>
+    </header>
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Profile = () => {
+    return <div className='content'>
+            <img src='http://wallpapers-image.ru/1920x1080/aircraft/wallpapers/wallpapers-aircraft-39.jpg'/>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+    </div>
+        }
