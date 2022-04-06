b1884e7179875c9a391cab3bcd9f20c728b3aec6
lui viton <artem_5511@mail.ru>
2022-04-07 01:00:59 +0300

    file system

---
+++ src/components/Profile/MyPosts/Myposts.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Myposts.module.css';
+import {Post} from "../posts/Post";
+
+export const Myposts = () => {
+    return <div className={s.mypost}>
+        My posts
+        <div>
+            <textarea>...</textarea>
+            <button>Add post</button>
+        </div>
+            <div className={s.posts}>
+                <Post message='Hi, how are you?' likesCount='7'/>
+                <Post message='It my first post' likesCount='23'/>
+                <Post/>
+                <Post/>
+            </div>
+    </div>
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Profile.module.css';
+import {Myposts} from './MyPosts/Myposts';
+
+export const Profile = () => {
+    return <div className={s.content}>
+        <img src='http://wallpapers-image.ru/1920x1080/aircraft/wallpapers/wallpapers-aircraft-39.jpg'/>
+        <div>ava+description</div>
+        <Myposts/>
+    </div>;
+};
