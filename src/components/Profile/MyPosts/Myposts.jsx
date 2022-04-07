89c7b78a73bfd73ffd594e960014ddf4b2914834
lui viton <artem_5511@mail.ru>
2022-04-08 00:32:13 +0300

    props

--- src/components/Profile/MyPosts/Myposts.jsx
+++ src/components/Profile/MyPosts/Myposts.jsx
@@ -8,20 +8,10 @@ export const Myposts = () => {
         <div>
             <textarea>...</textarea>
             <button>Add post</button>
-<<<<<<< HEAD
         </div>
             <div className={s.posts}>
                 <Post message='Hi, how are you?' likesCount='7'/>
                 <Post message='It my first post' likesCount='23'/>
-                <Post/>
-                <Post/>
             </div>
-=======
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
         </div>
->>>>>>> cb41539fb60521531f7b23e7443fb481b9eadb97
-    </div>
 }
