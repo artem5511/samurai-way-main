1b97b1a0e88b4c741ca807b9e2b7a0d372bc9a13
lui viton <artem_5511@mail.ru>
2022-09-06 22:18:49 +0300

    Revert "Revert "props""
    This reverts commit d1aedab16f92357c6359329cc848969e8cd3ed8f.

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,20 @@ import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 
-const App = (props: any) => {
+const App = (props: {
+    messages: any;
+    dialogs: any;
+    posts: any[]; }) => {
+
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path={'/dialogs'} component={Dialogs}/>
-                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/dialogs'} render={ () => (<Dialogs dialogs={props.dialogs} messages={props.messages}/>) } />
+                    {/*<Route path={'/dialogs'} component={Dialogs}/>*/}
+                    <Route path={'/profile'} render={ () => (<Profile posts={props.posts}/>)} />
                     <Route path={'/news'} component={News}/>
                     <Route path={'/music'} component={Music}/>
                     <Route path={'/settings'} component={Settings}/>
